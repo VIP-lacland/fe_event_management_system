a3159c4b497fd4c1bc6f558ea5f4f0270477936e
Bùi Tiến Lạc <lacvn2468@gmail.com>
2026-05-18 01:23:04 +0700

    Remove AppRoutes component and its dependencies

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,1 @@
-import { GuestRoutes } from "./GuestRouter"
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom"
 
-export function AppRoutes() {
-    return (
-        <HashRouter>
-                <Routes>
-                    <Route path="/*" element={<GuestRoutes />} />
-                    {/* Private Routes - cho user đã login (thêm sau) */}
-                    {/* <Route path="/*" element={<PrivateRoutes />} /> */}
-                    <Route path="*" element={<Navigate to="/login" replace />} />
-                </Routes>
-        </HashRouter>
-    )
-}
