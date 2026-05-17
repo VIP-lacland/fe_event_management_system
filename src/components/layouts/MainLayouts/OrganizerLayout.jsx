b30dbfe4b55b1d4a898e5b9e5ff1d94815de308c
lacBTPN27 <lacvn2468@gmail.com>
2026-05-17 18:12:03 +0700

    fix organizerLayout add outlet

--- src/components/layouts/MainLayouts/OrganizerLayout.jsx
+++ src/components/layouts/MainLayouts/OrganizerLayout.jsx
@@ -1,6 +1,5 @@
 // src/components/layouts/MainLayouts/OrganizerLayout.jsx
 import { Outlet } from "react-router-dom";
-// import OrganizerHeader from "../../organizer/Header/OrganizerHeader";
 import OrganizerHeader from "../../layouts/Header/OrganizerHeader";
 
 
@@ -9,7 +8,7 @@ export default function OrganizerLayout() {
     <div className="organizer-app">
       <OrganizerHeader />
       <main className="organizer-main">
-        {console.log('🔌 Outlet rendering')}
+        {console.log(' Outlet rendering')}
         <Outlet />
       </main>
     </div>
